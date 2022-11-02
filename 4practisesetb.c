d258949dbbf948f3afc1cdb05a787290b232f882
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-11-02 19:48:56 +0530

    Add files via upload

---
+++ 4practisesetb.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float pi=3.14;
+    int r;
+    printf("enter radius of circle :\n");
+    scanf("%d",&r);
+    printf("area of circle is %f \n",pi*r*r);
+    return 0;
+}
