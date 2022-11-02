765fff499980027f25eb8bc59bb33187c48af3e0
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-11-02 19:48:34 +0530

    Add files via upload

---
+++ 4practiceset.c
@@ -0,0 +1,13 @@
+// to print area of rectangle 
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter a :\n");
+    scanf("%d",&a);
+    printf("enter value of b:\n");
+    scanf("%d",&b);
+    c=a*b;
+    printf("the area of rectabgle is %d",c);
+}
