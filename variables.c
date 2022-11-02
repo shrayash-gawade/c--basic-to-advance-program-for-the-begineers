c14b42e5093a1057e22f421685972c11e8c11ec4
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-11-02 18:11:47 +0530

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=45;
+    float b=7.5;
+    char c='g';
+    printf("the value of a is %d ",a);
+    printf("the value of b is %f ",b);
+    printf("the value of c is %c",c);
+
+    return 0;
+}
