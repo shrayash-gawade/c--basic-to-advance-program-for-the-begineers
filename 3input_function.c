132d46477a803e4b90878d53e9a4648b0cfcff14
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-11-03 21:24:45 +0530

    Add files via upload

---
+++ 3input_function.c
@@ -0,0 +1,25 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b;
+//     printf("enter value of a");
+//     scanf("%d",&a);
+//     printf("enter the value of b:");
+//     scanf("%d",&b);
+//     printf("the sum of a and b is %d \n",a+b);
+//     return 0;
+
+// }
+
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter value of a:");
+    scanf("%d",&a);
+    printf("enter value of b:");
+    scanf("%d",&b);
+    printf("the sum of a and b is : %d",a+b);
+    return 0;
+}
