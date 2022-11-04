94f339f3b035cc293560c86a637cbf0bafc928e1
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-11-04 19:45:27 +0530

    Add files via upload

---
+++ areaofrectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,a;
+    printf("enter the length :");
+    scanf("%d",&l);
+    printf("enter bredth of rectangle :");
+    scanf("%d",&b);
+    a=l*b;
+    printf("the area of rectangle is : %d",a);
+    return 0;
+    
+}
