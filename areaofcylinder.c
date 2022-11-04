7754f1ff3cd332f893965e666e60c21f0a15c6c1
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-11-04 20:05:35 +0530

    Add files via upload

---
+++ areaofcylinder.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float r,h,a,pi;
+    pi=22/7;
+    a=pi*r*r*h;
+    printf("enter radius of cylinder :");
+    scanf("%f",&r);
+    printf("enter height of cylinder : ");
+    scanf("%f",&h);
+    printf("the area of cylinder is : %f ", a);
+    return 0;
+
+
+}
